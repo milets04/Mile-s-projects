f8152092e5fd13e56399d25e5e8ae159893bed87
Milena <miletorrico0041@gmail.com>
2025-09-23 20:48:22 -0400

    ejercicios base

---
+++ CompareNumbers.js
@@ -0,0 +1,7 @@
+let num1 = window.prompt("Enter the first number:");
+let num2 = window.prompt("Enter the second number:");
+if (num1>num2){
+    alert("The first number is larger.");
+}else{
+    alert("The second number is larger.");
+}
